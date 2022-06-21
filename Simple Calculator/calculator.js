0a56f5148db5c88b5b1f583edce75050add40aa2
Mally13 <mallymutuku13@gmail.com>
2022-06-21 17:49:26 +0300

    NaN

--- Simple Calculator/calculator.js
+++ Simple Calculator/calculator.js
@@ -1,48 +1,48 @@
 while (true){
 
-//operand 1 input
-const fNumber=parseFloat(prompt('Enter the first number: '));
-
-//operator input
-const operator=prompt('Enter operator (either + , - , * or / ): ');
-
-//operand 2 input
-const sNumber=parseFloat(prompt('Enter the second number: '));
-
-
-//calculations
-let result;
-
-if(operator=='+'){
-    result= fNumber + sNumber;
-    displayResult();
-    break;
-}
-else if(operator=='-'){
-    result=fNumber-sNumber;
-    displayResult();
-    break;
-}
-else if(operator=='*'){
-    result=fNumber*sNumber;
-    displayResult();
-    break;
-}
-else if(operator=='/'){
-    result=fNumber/sNumber
-    displayResult();
-    break;
+    //operand 1 input
+    const fNumber=parseFloat(prompt('Enter the first number: '));
     
-
-}else{
-    alert('You have not typed a valid operator, please run the program again.');
-
-}
-
-// display the result
-function displayResult(){
-    alert(`${fNumber} ${operator} ${sNumber} = ${result} \nRefresh page to calculate again` );
-    console.log(`${fNumber} ${operator} ${sNumber} = ${result}`);
-
-}
-}
+    //operator input
+    const operator=prompt('Enter operator (either + , - , * or / ): ');
+    
+    //operand 2 input
+    const sNumber=parseFloat(prompt('Enter the second number: '));
+    
+    
+    //calculations
+    let result;
+    
+    if(operator=='+' && !isNaN(fNumber) && !isNaN(sNumber)){
+        result= fNumber + sNumber;
+        displayResult();
+        break;
+    }
+    else if(operator=='-' && !isNaN(fNumber) && !isNaN(sNumber)){
+        result=fNumber-sNumber;
+        displayResult();
+        break;
+    }
+    else if(operator=='*' && !isNaN(fNumber) && !isNaN(sNumber)){
+        result=fNumber*sNumber;
+        displayResult();
+        break;
+    }
+    else if(operator=='/' && !isNaN(fNumber) && !isNaN(sNumber)){
+        result=fNumber/sNumber
+        displayResult();
+        break;
+        
+    
+    }else{
+        alert('You have not typed a valid operator or number, you have to try again.');
+    
+    }
+    
+    // display the result
+    function displayResult(){
+        alert(`${fNumber} ${operator} ${sNumber} = ${result} \nRefresh page to calculate again` );
+        console.log(`${fNumber} ${operator} ${sNumber} = ${result}`);
+    
+    }
+    }
